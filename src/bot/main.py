396effe20d680d83f4288ad0af76268699278e4f
Kirill Popov <diplomatgmg@gmail.com>
2025-05-13 02:33:13 +0300

    fix: drop updates if not debug
    Took 5 minutes

--- src/bot/main.py
+++ src/bot/main.py
@@ -8,6 +8,7 @@ from bot.core.loader import bot, dp
 from bot.handlers import register_handlers_routers
 from bot.keyboards import default_commands
 from bot.middlewares import register_middlewares
+from common.environment.config import env_config
 from common.logging.logger import setup_logging
 from common.sentry.setup import setup_sentry
 
@@ -45,6 +46,9 @@ async def main() -> None:
         await redis_consumer_task
         await dp.emit_shutdown()
     else:
+        if not env_config.debug:  # Не нужно ждать запуска бота, чтобы отправлять команды
+            await bot.delete_webhook(drop_pending_updates=True)
+
         await dp.start_polling(bot)
 
 
