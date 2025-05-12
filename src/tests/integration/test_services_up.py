d68555f1df68781894ce5ea6fab7cb1c4f8cc9f2
Kirill Popov <diplomatgmg@gmail.com>
2025-05-13 00:21:02 +0300

    wip: test bad tests
    Took 2 minutes

--- src/tests/integration/test_services_up.py
+++ src/tests/integration/test_services_up.py
@@ -7,4 +7,4 @@ from common.redis.engine import get_redis_instance
 async def test_redis_connection() -> None:
     client = await get_redis_instance()
     pong = await client.ping()
-    assert pong is True
+    assert pong is False
