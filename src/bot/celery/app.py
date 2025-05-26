25b8cb16f9c177cc02b4e745c23636c2eb7a8dab
Kirill Popov <diplomatgmg@gmail.com>
2025-05-26 20:19:21 +0300

    feat: added logging for celery
    Took 23 minutes

--- src/bot/celery/app.py
+++ src/bot/celery/app.py
@@ -1,8 +1,16 @@
 import asyncio
+from typing import Any
 
 from celery import Celery  # type: ignore[import-untyped]
+from celery.signals import setup_logging as celery_logging  # type: ignore[import-untyped]
 
 from bot.celery.config import celery_config
+from common.logging.setup import setup_logging
+
+
+@celery_logging.connect  # type: ignore[misc]
+def setup_celery_logging(**_: Any) -> None:
+    setup_logging("celery")
 
 
 __all__ = [
